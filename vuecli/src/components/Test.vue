96436dbb94a71f9d922596b92ec7db55456aa8a5
ASxx <limengyao@limengyaodeMacBook-Pro.local>
2018-03-10 20:43:14 +0800

    vuecli的一个test组件的搭建

---
+++ vuecli/src/components/Test.vue
@@ -0,0 +1,19 @@
+<template>
+  <h1 class="test">{{msg}}</h1>
+</template>
+
+<script>
+export default {
+  data(){
+      return {
+          msg: '测试vue-router'
+      }
+  }
+}
+</script>
+
+<style scoped>
+.test{
+    background: red;
+}
+</style>
