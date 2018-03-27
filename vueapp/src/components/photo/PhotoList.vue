72cd8ec76c9fb51ea98715b4f15a982f1895f479
LMY19961101 <844401944@qq.com>
2018-03-27 17:27:12 +0800

    vueapp5

---
+++ vueapp/src/components/photo/PhotoList.vue
@@ -0,0 +1,37 @@
+<template>
+  <ul class="list">
+      <li v-for="(photo, index) in photoList" :key="index">
+          <img :src="photo.src" alt="">
+      </li>
+  </ul>
+</template>
+
+<script>
+import Axios from 'axios'
+export default {
+    data () {
+        return {
+            photoList: []
+        };
+    },
+  mounted () {
+    Axios.get("/static/data/photodata.json").then(res => {
+        console.log(res);
+        this.photoList = res.data.photoData;
+    }).catch(err => {})  
+  }
+}
+</script>
+
+<style scoped>
+.list{
+    margin: 1rem 0;
+}
+.list li{
+    width: 50%;
+    float: left;
+}
+.list img{
+    width: 100%;
+}
+</style>
