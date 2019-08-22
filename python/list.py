d5489966edadf7c51393995a14083c39c43928fa
李梦瑶 <limengyao17@jrmyv921jv3n.jdjr.360buyad.local>
2019-08-22 22:01:33 +0800

    python

---
+++ python/list.py
@@ -0,0 +1,11 @@
+# classmates = ['a', 'b', 'c']
+# print(classmates)
+
+# L = [
+#     ['Apple', 'Google', 'Microsoft'],
+#     ['Java', 'Python', 'Ruby', 'PHP'],
+#     ['Adam', 'Bart', 'Lisa']
+# ]
+
+# print(L[2][2])
+
