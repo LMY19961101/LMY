db5e6c4728aa9df77013921c0d28ebc6469e8a02
李梦瑶 <limengyao17@jrmyv921jv3n.jdjr.360buyad.local>
2019-09-04 20:28:58 +0800

    python

---
+++ python/dict.py
@@ -0,0 +1,51 @@
+# d = {'a':1, 'b':2, 'c':3}
+# print(d['a'])
+# d['d'] = 4
+# print(d['d'])
+# d['a'] = 5
+# print(d['a'])
+# print(d['e'])
+# print('e' in d)
+
+# d.get('e')
+# print(d.get('e'), -1)
+# d.pop('a')
+# print(d)
+
+# key = [1,2,3]
+# d[key] = 'a list'
+# s = set(['a','a', 2, 3])
+# print(s)
+
+# s.add(4)
+# # print(s)
+
+# s.remove(4)
+# print(s)
+
+# s1 = set([1, 2, 3])
+# s2 = set([2, 3, 4])
+# # print(s1&s2)
+# print(s1|s2)
+
+# a = ['c', 'b', 'a']
+# a.sort()
+# print(a)
+
+# a = 'abc'
+# a.replace('a', 'A')
+# print(a.replace('a', 'A'))
+# print(a)
+
+# a = 'abc'
+# b = a.replace('a', 'A')
+# print(a)
+# print(b)
+
+s = (1, 2, 3)
+s1 = (1, [2, 3])
+d = {s: s1}
+# print(d)
+# s2 = set([s, s1])
+print(s1, s)
+# print(s2)
---
+++ python/function.py
@@ -0,0 +1,41 @@
+#encoding:utf-8
+
+# a = abs(100)
+# print(a)
+
+# b = abs(-20)
+# print(b)
+
+# c = abs(12.34)
+# print(c)
+
+# d = max(1, -3, 90)
+# print(d)
+
+# print(int('123'))
+# print(int(12.34))
+# print(float('12.34'))
+# print(str(1.23))
+# print(str(100))
+# print(bool(1))
+# print(bool(''))
+
+# a = abs
+# print(a(-1))
+
+# n1 = 255
+# n2 = 1000
+# print(hex(n2))
+
+# n1 = hex(int(input("请输入第一组数字：")))
+# n2 = hex(int(input("请输入第二组数字：")))
+# print("第一组转换为十六进制为：%s\n第二组为：%s"%(n1, n2))
+
+# def my_abs(x):
+#   if (x >= 0):
+#     return x
+#   else:
+#     return -x
+# print(my_abs(-1))
+
+
